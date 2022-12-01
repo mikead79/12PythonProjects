9ef9e33bb4d7c14c5109079779e076ce69f91530
Mihai <mikead79@yahoo.com>
2022-12-01 20:10:00 +0100

    Eliminate the test print of the word you have to guess

--- hangman.py
+++ hangman.py
@@ -6,7 +6,6 @@ def get_valid_word(words):
     word = random.choice(words)
     while '-' in word or ' ' in word:
         word = random.choice(words)
-    print(word)
     return word.upper()
 
 def hangman():
